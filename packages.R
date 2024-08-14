cd41230aa8ec2bfa4855d0e969d864a8867ac44e
Danielle Evans <37087457+de84sussex@users.noreply.github.com>
2024-08-14 12:27:46 +0100

    Create packages.R

---
+++ packages.R
@@ -0,0 +1,77 @@
+packages <- c("afex",
+              "BayesFactor",
+              "broom",
+              "broom.mixed",
+              "car",
+              "devtools",
+              "easystats",
+              "emmeans",
+              "flextable",
+              "GGally",
+              "GPArotation",
+              "gt",
+              "ggfortify",
+              "here",
+              "interactions",
+              "Hmisc",
+              "htmltools",
+              "jtools",
+              "kableExtra",
+              "lavaan",
+              "lme4",
+              "lmerTest",
+              "lubridate",
+              "psych",
+              "qqplotr",
+              "nlme",
+              "remotes",
+              "rempsyc",
+              "robust",
+              "robustbase",
+              "sandwich",
+              "tidyverse",
+              "tidyr",
+              "WRS2")
+
+install.packages(packages)
+
+easystats::install_suggested()
+
+remotes::install_github("rstudio/learnr")
+
+remotes::install_github("profandyfield/discovr")
+
+remotes::install_github("crsh/papaja")
+
+# Fun Stuff
+remotes::install_github("rstudio/gradethis")
+
+devtools::install_github("jhollist/dadjokeapi")
+
+install.packages("rwhatsapp")
+
+remotes::install_github("R-CoderDotCom/ggdogs@main")
+
+#setRepositories(ind=1:2)
+devtools::install_github("GuangchuangYu/ggimage")
+
+remotes::install_github("R-CoderDotCom/ggcats@main")
+
+install.packages("dragracer")
+
+remotes::install_github("rladies/praise")
+
+install.packages("cowsay")
+
+devtools::install_github("sctyner/memer")
+
+install.packages("RColorBrewer")
+
+install.packages("tidytext")
+
+install.packages("stopwords")
+
+devtools::install_github("schochastics/Rokemon")
+
+remotes::install_github("williamorim/pokemon")
+
